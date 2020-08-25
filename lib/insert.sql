e0562dd36fb0f3128a29abeba67c1fb250830424
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-25 02:24:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", 20, M, brown, calm);
+    VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Rowdy", 10, M, yellow, agressive);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
