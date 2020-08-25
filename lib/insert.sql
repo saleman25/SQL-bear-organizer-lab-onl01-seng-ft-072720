ece2a6a443ba1dafc7c04630bc7e5dde6cf1f998
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-25 02:20:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO bears (name, grade, gpa, tardies)
-    VALUES ("John", 11, 3.4, 3);
-INSERT INTO bears (name, grade, gpa, tardies)
-    VALUES ("Kareem", 10, 2.4, 12);
-INSERT INTO bears (name, grade, gpa, tardies)
-    VALUES ("Zak", 12, 4.0, 0);
-INSERT INTO bears (name, grade, gpa, tardies)
-    VALUES ("Kim", 11, 2.0, 9);
-INSERT INTO bears (name, grade, gpa, tardies)
-    VALUES ("Bob", 10, 1.9, 17);
-INSERT INTO bears (name, grade, gpa, tardies)
-    VALUES ("Stef", 9, 2.5, 2);
-INSERT INTO bears (name, grade, gpa, tardies)
-    VALUES ("George", 9, 3.9, 1);
-INSERT INTO bears (name, grade, gpa, tardies)
-    VALUES ("Daryl", 9, 3.5, 5);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 20, male, brown);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 5, male, yellow);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 12, female, orange);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sergeant Brown", 22, female, brown);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 7, female, black);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 15, male, green);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 9, female, white);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("null", 8, male, brown);
